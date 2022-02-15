a0d8212157e70b22b8478142b0538281dd3335e5
jola7373 <jola7373@naver.com>
2022-02-15 16:54:43 +0900

    feat: creating comment entity

---
+++ src/main/java/com/qnnect/comments/domain/Comments.java
@@ -0,0 +1,26 @@
+package com.qnnect.comments.domain;
+
+import com.qnnect.user.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Comments {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+//    @ManyToOne
+//    @JoinColumn(name="")
+
+}
