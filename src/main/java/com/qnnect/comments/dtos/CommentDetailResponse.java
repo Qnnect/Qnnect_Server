cfc8a6cd4267a6697928db6e82a3c67329485368
jola7373 <jola7373@naver.com>
2022-02-18 12:18:56 +0900

    feat: create comment related api lists

---
+++ src/main/java/com/qnnect/comments/dtos/CommentDetailResponse.java
@@ -0,0 +1,27 @@
+package com.qnnect.comments.dtos;
+
+import com.qnnect.comments.domain.Reply;
+import com.qnnect.user.domain.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@ApiModel(value="댓글(대댓글 포함)")
+@Getter
+public class CommentDetailResponse {
+
+    @ApiModelProperty(value = "댓글", example = "지민이는 저번부터 싱가폴 얘기하더니 " +
+            "진짜 가고싶나보네ㅋㅋㅋ")
+    private String content;
+
+//    @ApiModelProperty(value = "댓글", example = "지민이는 저번부터 싱가폴 얘기하더니 " +
+//            "진짜 가고싶나보네ㅋㅋㅋ")
+    private User user;
+
+
+    private List<ReplyResponse> replies;
+
+
+}
