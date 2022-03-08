b6ce3a01f85d0d269a37c738fd0455e132cc49f2
jola7373 <jola7373@naver.com>
2022-03-08 16:36:29 +0900

    fix: add diaryColor in cafeDetailResponse

--- src/main/java/com/qnnect/cafe/dto/CafeDetailResponse.java
+++ src/main/java/com/qnnect/cafe/dto/CafeDetailResponse.java
@@ -3,6 +3,7 @@ package com.qnnect.cafe.dto;
 import com.qnnect.cafe.domain.Cafe;
 
 import com.qnnect.cafe.domain.CafeUser;
+import com.qnnect.cafe.domain.EDiaryColor;
 import com.qnnect.questions.domain.CafeQuestion;
 import com.qnnect.user.domain.User;
 import com.qnnect.user.dtos.ProfileResponse;
@@ -25,6 +26,8 @@ public class CafeDetailResponse {
     
     private String code;
 
+    private EDiaryColor diaryColor;
+
     private CafeUserResponse currentUserResponse;
 
     private List<CafeUserResponse> cafeUserResponseList;
@@ -36,6 +39,7 @@ public class CafeDetailResponse {
         this.title = entity.getTitle();
         this.isOrganizer = currentCafeUser.getUser() == entity.getOrganizer();
         this.code = entity.getCode();
+        this.diaryColor = entity.getDiaryColor();
         this.currentUserResponse = CafeUserResponse.from(currentCafeUser);
         this.cafeQuestionResponseList = CafeQuestionResponse.listFrom(entity.getCafeQuestions());
         this.cafeUserResponseList = CafeUserResponse.listFrom(entity.getCafeUsers(), currentCafeUser);
--- src/main/java/com/qnnect/questions/apis/QuestionController.java
+++ src/main/java/com/qnnect/questions/apis/QuestionController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Api(tags = {"질문 관련 API"})
 public class QuestionController {
 
-//    @GetMapping("/questions")
+//    @PMapping("/questions")
 //    @ApiOperation(value = "오늘의 질문")
 //    public ResponseEntity<TodayQuestionResponse> getQuestionToday(@RequestParam String searchWord){
 //
