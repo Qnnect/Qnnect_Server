2e44ed589107b0dffc93dd08775887bdf0f42d7c
jola7373 <jola7373@naver.com>
2022-03-30 18:56:23 +0900

    Revert "feat: get question waiting list"
    This reverts commit 0e6b3e5de01592a907b4781839674acc30b06d9a.

--- src/main/java/com/qnnect/questions/service/QuestionService.java
+++ src/main/java/com/qnnect/questions/service/QuestionService.java
@@ -1,7 +1,4 @@
 package com.qnnect.questions.service;
 
-import com.qnnect.questions.dto.WaitingListQuestionResponse;
-
 public interface QuestionService {
-    public WaitingListQuestionResponse get(long questionId);
 }
